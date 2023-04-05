4871e6cd8a1fad5be1dc53ee2df44e410631ae29
Alexandre <alx.delira@gmail.com>
2023-04-04 22:36:30 -0400

    Mudanças no Mock

--- src/test/pessoa.spec.js
+++ src/test/pessoa.spec.js
@@ -1,11 +1,11 @@
 import { describe, expect, it, jest } from "@jest/globals";
 import pessoas from "../models/Pessoa.js";
-import PessoaControlle from "../controllers/PessoaController.js"
+import PessoaController from "../controllers/PessoaController.js"
 
 describe("Deve Retornar os testes Unitarios de Pessoas", () => {
     const dataPessoa = new Date()
     const objPessoa = {
-        nome: "Alexandre Nogueira",
+        nome: "Alexandre",
         cpf: "008",
         nit: "87238378237",
         dataNascimento: dataPessoa,
@@ -30,26 +30,58 @@ describe("Deve Retornar os testes Unitarios de Pessoas", () => {
 
     it("Deve Retornar o cadastro simulabdo com mock", () => {
         const pessoa = new pessoas(objPessoa)
-        PessoaControlle.cadastrarPessoa = jest.fn().mockReturnValue({
-
-            nome: "Alexandre Nogueira",
-            cpf: "008",
-            nit: "87238378237",
-            dataNascimento: dataPessoa,
-            estrangeiro: "sim",
-            pais: "Reoulplica só de Xecas",
-            cep: "123456",
-            logradouro: "rua rua rua",
-            numero: "2345",
-            bairro: "tamandaré",
-            cidade: "Guajará-Mirim",
-            estado: "Bolis",
-            telefone: "690090090",
-            telefoneContato: "9098876688"
-        })
-        const retorno = PessoaControlle.cadastrarPessoa();
-        expect(retorno.nome).toEqual("Alexandre Nogueira")
-        expect(retorno).toHaveProperty("nome","Alexandre Nogueira")
+        PessoaController.listarPessoas = jest.fn().mockReturnValue(
+            {
+                nome: "Alexandre",
+                cpf: "008",
+                nit: "87238378237",
+                dataNascimento: dataPessoa,
+                estrangeiro: "sim",
+                pais: "Reoulplica só de Xecas",
+                cep: "123456",
+                logradouro: "rua rua rua",
+                numero: "2345",
+                bairro: "tamandaré",
+                cidade: "Guajará-Mirim",
+                estado: "Bolis",
+                telefone: "690090090",
+                telefoneContato: "9098876688"
+            },
+            {
+                nome: "Mateus",
+                cpf: "008",
+                nit: "87238378237",
+                dataNascimento: dataPessoa,
+                estrangeiro: "sim",
+                pais: "Reoulplica só de Xecas",
+                cep: "123456",
+                logradouro: "rua rua rua",
+                numero: "2345",
+                bairro: "tamandaré",
+                cidade: "Guajará-Mirim",
+                estado: "Bolis",
+                telefone: "690090090",
+                telefoneContato: "9098876688"
+            },
+            {
+                nome: "teo",
+                cpf: "008",
+                nit: "87238378237",
+                dataNascimento: dataPessoa,
+                estrangeiro: "sim",
+                pais: "Reoulplica só de Xecas",
+                cep: "123456",
+                logradouro: "rua rua rua",
+                numero: "2345",
+                bairro: "tamandaré",
+                cidade: "Guajará-Mirim",
+                estado: "Bolis",
+                telefone: "690090090",
+                telefoneContato: "9098876688"
+            })
+        const retorno = PessoaController.listarPessoas();
+        console.log(retorno)
+        expect(retorno).toHaveProperty("nome","Alexandre")
     });
 
 
